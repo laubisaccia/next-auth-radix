4087a4d440b2c854bc5af6050dfb77c7e146f14c
Maria Laura <Maria.Laura.Bisaccia1@ibm.com>
2025-07-08 17:20:29 -0300

    proj setup

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+import {BookmarkIcon} from '@radix-ui/react-icons'
+function LoginPage() {
+  return (
+    <div>LoginPage
+        <Button>
+            <BookmarkIcon/>
+	Login
+</Button>
+
+    </div>
+  )
+}
+
+export default LoginPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Theme>{children}</Theme>
       </body>
     </html>
   );
